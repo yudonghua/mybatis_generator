d1677fc1a229370b3b356ce8b19fae5a8e7b578c
yudonghua <723948277@qq.com>
2017-05-27 13:17:41 +0800

    auto

---
+++ src/lcw/dao/CustomerMapper.java
@@ -0,0 +1,36 @@
+package lcw.dao;
+
+import java.util.List;
+import lcw.model.Customer;
+import lcw.model.CustomerExample;
+import org.apache.ibatis.annotations.Param;
+
+public interface CustomerMapper {
+    long countByExample(CustomerExample example);
+
+    int deleteByExample(CustomerExample example);
+
+    int deleteByPrimaryKey(String username);
+
+    int insert(Customer record);
+
+    int insertSelective(Customer record);
+
+    List<Customer> selectByExampleWithBLOBs(CustomerExample example);
+
+    List<Customer> selectByExample(CustomerExample example);
+
+    Customer selectByPrimaryKey(String username);
+
+    int updateByExampleSelective(@Param("record") Customer record, @Param("example") CustomerExample example);
+
+    int updateByExampleWithBLOBs(@Param("record") Customer record, @Param("example") CustomerExample example);
+
+    int updateByExample(@Param("record") Customer record, @Param("example") CustomerExample example);
+
+    int updateByPrimaryKeySelective(Customer record);
+
+    int updateByPrimaryKeyWithBLOBs(Customer record);
+
+    int updateByPrimaryKey(Customer record);
+}
